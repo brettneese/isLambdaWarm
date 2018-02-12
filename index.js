286fcab4aac69227745d7aec9e1d67c05ed4342d
Brett Neese <brett@neese.rocks>
2018-02-12 13:10:30 -0800

    set the lamba warmed date outside of the block, also fix date parsing

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var fs = require('fs');
+var lambdaWarm;
+const timeWarmed = Date.now();
 
 module.exports = function (cb) {
-    // this is, of course, going to depend on when you actually load this module
-    const timeWarmed = Date.now();
-    
+    // this is, of course, going to depend on when you actually load this module    
     fs.readFile('/tmp/lambdaWarm', '' , function (err, data) {
         if (err && err.code == 'ENOENT') {
             fs.writeFile('/tmp/lambdaWarm', timeWarmed, function (err) {
                 if (err) {
                     return cb(err);
                 }
-                return cb(null, false, timeWarmed);
+                return cb(null, false, new Date(parseInt(timeWarmed)));
             })
         } else {
             return cb(null, true, new Date(parseInt(data)))
