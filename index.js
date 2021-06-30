5f74f4d67590736ac4e9d37ed4fbdb835eaa001d
Brett Neese <brneese@brneese.com>
2021-06-30 13:39:18 -0500

    patch for newer nodes

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-var fs = require('fs');
+var fs = require("fs");
 var lambdaWarm;
 const timeWarmed = Date.now();
 
 module.exports = function (cb) {
-    // this is, of course, going to depend on when you actually load this module    
-    fs.readFile('/tmp/lambdaWarm', '' , function (err, data) {
-        if (err && err.code == 'ENOENT') {
-            fs.writeFile('/tmp/lambdaWarm', timeWarmed, function (err) {
-                if (err) {
-                    return cb(err);
-                }
-                return cb(null, false, new Date(parseInt(timeWarmed)));
-            })
-        } else {
-            return cb(null, true, new Date(parseInt(data)))
+  // this is, of course, going to depend on when you actually load this module
+  fs.readFile("/tmp/lambdaWarm", "", function (err, data) {
+    if (err && err.code == "ENOENT") {
+      fs.writeFile("/tmp/lambdaWarm", parseInt(timeWarmed, 10), function (err) {
+        if (err) {
+          return cb(err);
         }
-    })
-}
+        return cb(null, false, new Date(parseInt(timeWarmed)));
+      });
+    } else {
+      return cb(null, true, new Date(parseInt(data)));
+    }
+  });
+};
